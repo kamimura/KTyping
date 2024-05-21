8cece5e03d05dbc7352cdee53ea079bd5a84449d
kamimura <kamimura@live.jp>
2024-05-21 11:50:02 +0900

    Initial Commit

---
+++ KTyping/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  KTypingApp.swift
+//  KTyping
+//
+//  Created by kamimura on 2023/12/31.
+//
+
+import Cocoa
+import InputMethodKit
+
+// Necessary to launch this app
+class NSManualApplication: NSApplication {
+    private let appDelegate = AppDelegate()
+
+    override init() {
+        super.init()
+        self.delegate = appDelegate
+    }
+
+    required init?(coder: NSCoder) {
+        // No need for implementation
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var server = IMKServer()
+    var candidatesWindow = IMKCandidates()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+        server = IMKServer(name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String, bundleIdentifier: Bundle.main.bundleIdentifier)
+        candidatesWindow = IMKCandidates(server: server, panelType: kIMKSingleRowSteppingCandidatePanel, styleType: kIMKMain)
+        NSLog("tried connection")
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
